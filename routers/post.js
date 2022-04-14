654ee19453a540e43ba44a28258328723cce2db0
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-04-14 21:24:19 +0530

    changes

--- routers/post.js
+++ routers/post.js
@@ -34,7 +34,7 @@ router.get(
           await Post.findByIdAndUpdate(post._id, { active: false });
         }
       });
-      setTimeout(next, 1000);
+      next();
     } catch (err) {
       console.log(err);
       res.status(500).send({ status: false, error: err });
