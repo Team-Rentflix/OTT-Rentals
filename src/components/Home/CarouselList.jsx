49cfbe94476903a700d9d1d6212543177d7b1813
abhinavginti <abhinavginti@gmail.com>
2022-05-28 22:35:21 +0530

    minor changes

--- src/components/Home/CarouselList.jsx
+++ src/components/Home/CarouselList.jsx
@@ -3,9 +3,9 @@ import Image2 from '../Images/Carousel/24-rr-vs-gt-1-1024x614.png'
 
 
 export const CarouselList = [
+    Image2,
     'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/8084/1258084-h-5a45792c0366',
     Image1,
-    Image2,
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO_Z8dfz1znTcx6e-hQMoPbm0k0rPJAsIshA&usqp=CAU',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO_Z8dfz1znTcx6e-hQMoPbm0k0rPJAsIshA&usqp=CAU',
     'https://newsroompost.com/wp-content/uploads/2022/05/Untitled-design-1-85.jpg'
 ]
