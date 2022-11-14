729810e8d4fa36bb442884185d6ba89ba2ff686e
abhinavginti <abhinavginti@gmail.com>
2022-11-14 14:14:34 +0530

    url fix

--- src/components/APICall.js
+++ src/components/APICall.js
@@ -1,5 +1,5 @@
 export default async function APICall(_path, _method, _body) {
-    const serverURL = 'https://rentflix-server.herokuapp.com/'
+    const serverURL = 'https://rentflix-server.herokuapp.com'
     const response = await fetch(serverURL + _path, {
         method: _method,
         headers: {
