6d12d810e689129c85f650dbde45c77e7ae3e75c
abhinavginti <abhinavginti@gmail.com>
2022-11-14 15:34:00 +0530

    getpass route added

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,4 +1,8 @@
 const Transaction = require('../models/transaction.model')
+const jwt = require('jsonwebtoken')
+const Post = require('../models/post.model')
+
+require("dotenv").config({ path: "./config.env" });
 
 exports.storeTransactionData = async (req, res) => {
     try {
@@ -7,6 +11,27 @@ exports.storeTransactionData = async (req, res) => {
         res.status(200).send({ status: true })
     } catch (err) {
         console.log(err);
+        res.status(500).send({ status: false, error: err })
+    }
+}
+
+exports.decryptPassword = async (req, res) => {
+    try {
+        const post = await Post.findById(req.query.post_id)
+        if (!post) throw "No Post Found"
+        console.log(post)
+        if (jwt.verify(post.secret_key, process.env.SECRET)) {
+            const {secret_key} = jwt.decode(post.secret_key)
+            if(jwt.verify(post.acc_pass,secret_key)){
+                const {acc_pass} = jwt.decode(post.acc_pass)
+                res.send({status:true, password: acc_pass})
+            }else{
+                throw "Secret Key Invalid"
+            }
+        }else{
+            throw "Some Error Occured"
+        }
+    } catch (err) {
         res.status(500).send({ status: false, error: err })
     }
 }
--- routers/transaction.js
+++ routers/transaction.js
@@ -4,5 +4,6 @@ const auth = require('../libs/auth')
 const transactionController = require('../controllers/transactionController')
 
 router.post('/transaction-success', auth, transactionController.storeTransactionData)
+router.get('/getPass',auth,transactionController.decryptPassword)
 
 module.exports = router
